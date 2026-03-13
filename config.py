104c84cb21402558308044ec4b87aae546fc2e63
Luiza Bissoni <luiza.bissoni@agcapital.local>
2026-03-13 10:47:24 -0300

    Removendo senha do código

--- config.py
+++ config.py
@@ -2,11 +2,16 @@
 # DADOS DO BANCO
 # =========================
 
-DB_HOST = "oci-agnes-prd.agnes.com.br"
-DB_NAME = "esocial_ap"
-DB_USER = "luiza_bissoni"
-DB_PASSWORD = "evMZDeUs11r8U4pCSvaInvd2"
-DB_PORT = "5432"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
 
 # =========================
 # CNPJS (RAIZ)
@@ -25,7 +30,7 @@ CNPJS = [
 # formato AAAA-MM
 # =========================
 
-DATA_INICIO = "2022-01"
+DATA_INICIO = "2025-06"
 DATA_FIM = "2026-03"
 
 
